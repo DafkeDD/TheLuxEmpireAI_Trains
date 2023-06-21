29aeade0d71e6185cf47530c0f54c261643932fc
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-06-21 04:08:47 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,30 +1,36 @@
-Config = {
-    Framework = 'qbcore', -- Choose 'qbcore' or 'esx' as per your preference
-    Tracks = {
-        {
-            TrackID = 0,
-            SpawnFrequency = 2 * 60 * 1000,
-            Trains = {
-                { Name = 'freight', Count = 1 },
-                { Name = 'freightcar', Count = 2 },
-                { Name = 'freightcont1', Count = 1 },
-            },
-            TimeDifference = 10 * 60 * 1000, -- 10 minutes in milliseconds
-            ForceDoorsOpen = true
+-- config.lua
+
+Config = {}
+
+-- Choose 'qbcore' or 'esx' as per your preference
+Config.Framework = 'qbcore'
+
+-- Define the tracks and their configurations
+Config.Tracks = {
+    {
+        TrackID = 0,
+        SpawnFrequency = 2 * 60 * 1000, -- 2 minutes in milliseconds
+        Trains = {
+            { Name = 'freight', Count = 1 },
+            { Name = 'freightcar', Count = 2 },
+            { Name = 'freightcont1', Count = 1 },
         },
-        {
-            TrackID = 3,
-            SpawnFrequency = 5 * 60 * 1000,
-            Trains = {
-                { Name = 'metrotrain', Count = 1 },
-                { Name = 'tankercar', Count = 1 },
-                { Name = 'freightgrain', Count = 1 },
-            },
-            TimeDifference = 10 * 60 * 1000, -- 10 minutes in milliseconds
-            ForceDoorsOpen = false
+        TimeDifference = 10 * 60 * 1000, -- 10 minutes in milliseconds
+        ForceDoorsOpen = true
+    },
+    {
+        TrackID = 3,
+        SpawnFrequency = 5 * 60 * 1000, -- 5 minutes in milliseconds
+        Trains = {
+            { Name = 'metrotrain', Count = 1 },
+            { Name = 'tankercar', Count = 1 },
+            { Name = 'freightgrain', Count = 1 },
         },
-        -- Add more tracks as needed
-    }
-    -- Add more options here as per your AI knowledge
-    -- Example: MaxSpeed = 100, NumPassengers = 50, etc.
+        TimeDifference = 10 * 60 * 1000, -- 10 minutes in milliseconds
+        ForceDoorsOpen = false
+    },
+    -- Add more tracks as needed
 }
+
+-- Add more options here as per your AI knowledge
+-- Example: Config.MaxSpeed = 100, Config.NumPassengers = 50, etc.
